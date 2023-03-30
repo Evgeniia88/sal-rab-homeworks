dc527e96b168d9d340520c03f360dbb02e3f2998
Parasika Vika <jeneve.nasunova@yandex.ru>
2023-03-30 14:43:21 +0300

    task 1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,9 +1,6 @@
 function addInCartMessage(title, price) {
-    let productName = title;
-    let productPrice = price;
-
-    
-
+let productName = title;
+let productPrice = price;
 let message;
 message = productName;
 message = message + " ";
@@ -13,37 +10,21 @@ message = message + "productPrice";
 message = message " ";
 message = message "теперь в корзине!";
 console.log(message);
-    
-    
-
-    return message;
+return message;
 }
-
 function addInCartCountChange(value) {
-
-   
-
 let oldValue = value;
-
 let newValue;
-
 newValue = oldValue;
-
 newValue = newValue + 1;
-
 }
-
 function addInCartSumChange(sum, delta) {
-    let oldSum = sum;
-    let difference = delta;
+let oldSum = sum;
+let difference = delta;
 
 let newSum = oldSum + difference
 let newSumText = newSum + ₽
 newSumText = ${newSum} ₽
-
-
-    
-
-    return newSumText;
+return newSumText;
 }
 
