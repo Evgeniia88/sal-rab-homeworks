2a094e83be4f517b776e20575112bd2b9131db0a
Parasika Vika <jeneve.nasunova@yandex.ru>
2023-04-05 20:33:57 +0300

    tas_2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,20 +5,16 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    // создайте переменную shippingSum
+    let shippingSum; // создайте переменную shippingSum
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) { // если productsSum равно 0, если productsSum Больше или равна freeShippingMinSum
 
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
+        shippingSum = 0; // то shippingSum присвоить значение 0
 
-    // если productsSum Больше или равна freeShippingMinSum,
-    // то shippingSum присвоить значение 0
+    } else { // если productsSum больше 0 и меньше freeShippingMinSum,
+        shippingSum  = shippingPrice;  // то shippingSum присвоить значение shippingPrice
 
-    // если productsSum больше 0 и меньше freeShippingMinSum,
-    // то shippingSum присвоить значение shippingPrice
+        return shippingSum;
 
-    // Конец решения задания №2.1.
-
-    return shippingSum;
 }
 
 function calcDiscount(sum, min, discount) {
@@ -28,9 +24,9 @@ function calcDiscount(sum, min, discount) {
 
     // Задание №2.2. Рассчитать скидку
 
-    // создайте переменную discountSum
+   let discountSum; // создайте переменную discountSum
 
-    // если productsSum больше или равно discountMinSum,
+    if productsSum >= discountMinSum; // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
 
