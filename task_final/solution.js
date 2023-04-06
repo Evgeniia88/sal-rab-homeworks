ff7d842adaf086fe4fe62e2292b3451ae787c75b
Parasika Vika <jeneve.nasunova@yandex.ru>
2023-04-06 17:57:52 +0300

    task_4 done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,32 +34,47 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+	let data = {
+		goods: [],
+		order: {}
+	};
 
-    let countOfGoods = goods.length;
+	let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    let data = {client: "", order: {address: "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв " + address.flat, sum: sum}, goods: []};
+	for (let i = 0; i <= countOfGoods; i += 1) {
+		data.goods.push(goods[i].title);
+		let data = {
+			client: "",
+			order: {
+				address: "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв " + address.flat,
+				sum: sum
+			},
+			goods: []
+		};
 
-        data.client = name + " " + phone;
-        data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв " + address.flat;
-        data.order.sum = sum;
-     let countOfGoods = goods.length;
-     for (let i = 0; i < countOfGoods; i ++) {
-      data.goods.push ({"title": goods[i].title, "count": goods[i].count});
-     } 
-    let jsonData = JSON.stringify({data:data});
+		data.client = name + " " + phone;
+		data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв " + address.flat;
+		data.order.sum = sum;
+		let countOfGoods = goods.length;
+		for (let i = 0; i < countOfGoods; i++) {
+			data.goods.push({
+				"title": goods[i].title,
+				"count": goods[i].count
+			});
+		}
+		let jsonData = JSON.stringify({
+			data: data
+		});
 
-    return jsonData;
-    }
+		return jsonData;
+	}
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+	data.order.address = address;
+	data.order.sum = name + phone + address + goods + sum;
 
-    data.client = 'Иван';
+	data.client = 'Иван';
 
-    let jsonData = JSON.stringify(data);
+	let jsonData = JSON.stringify(data);
 
-    return jsonData;
+	return jsonData;
 }
