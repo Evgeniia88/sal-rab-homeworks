9794282dfde5bb0496abbf1cdff25805bda8e1d8
Parasika Vika <jeneve.nasunova@yandex.ru>
2023-04-06 17:11:23 +0300

    taskfinal

--- task_final/solution.js
+++ task_final/solution.js
@@ -42,18 +42,18 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push(goods[i].title);
     let data = {client: "", order: {address: "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв " + address.flat, sum: sum}, goods: []};
 
-    data.client = name + " " + phone;
+        data.client = name + " " + phone;
         data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв " + address.flat;
         data.order.sum = sum;
      let countOfGoods = goods.length;
      for (let i = 0; i < countOfGoods; i ++) {
       data.goods.push ({"title": goods[i].title, "count": goods[i].count});
      } 
-        let jsonData = JSON.stringify({data:data});
+    let jsonData = JSON.stringify({data:data});
 
-        return jsonData;
+    return jsonData;
     }
-    
+
     data.order.address = address;
     data.order.sum = name + phone + address + goods + sum;
 
